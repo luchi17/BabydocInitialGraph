100655fbe1a83fd4bee2c02ccaa8274a0d3c7890
Luchi <luchi.parejo17@gmail.com>
2019-04-09 01:04:39 +0200

    Finally scroll view and graph included in home

---
+++ Babydoc/Controller/FirstViewController.swift
@@ -0,0 +1,42 @@
+//
+//  FirstViewController.swift
+//  Babydoc
+//
+//  Created by Luchi Parejo alcazar on 05/04/2019.
+//  Copyright © 2019 Luchi Parejo alcazar. All rights reserved.
+//
+
+import UIKit
+import Charts
+
+class FirstViewController: UIViewController {
+    
+    var iosData = PieChartDataEntry(value: 0)
+    var macData = PieChartDataEntry(value: 0)
+    var numberofDownloads = [PieChartDataEntry]()
+
+    @IBOutlet weak var piechart: PieChartView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        iosData.label = "ios"
+        macData.label="mac"
+        iosData.value = 30
+        macData.value = 50
+        numberofDownloads = [iosData,macData]
+        updatechartData()
+    }
+    func updatechartData(){
+        let chartdataset = PieChartDataSet(values: numberofDownloads, label: nil)
+        let chartdata = PieChartData(dataSet: chartdataset)
+        //give some colors
+        let colors = [UIColor.blue,UIColor.red]
+        chartdataset.colors = colors
+        piechart.data = chartdata
+        
+        
+    }
+
+
+}
+
---
+++ Babydoc/View/CustomRecentActivity/RecentActivity.swift
@@ -0,0 +1,30 @@
+//
+//  RecentActivity.swift
+//  Babydoc
+//
+//  Created by Luchi Parejo alcazar on 08/04/2019.
+//  Copyright © 2019 Luchi Parejo alcazar. All rights reserved.
+//
+
+import UIKit
+
+class RecentActivity: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
